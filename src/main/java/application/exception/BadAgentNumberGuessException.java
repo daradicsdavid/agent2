34d9e1c7246cbc8be2e91924d0d7aa5c125cad56
Daradics Dávid <ddaradics@alvicom.hu>
2018-05-07 15:42:01 +0200

    V9

--- src/main/java/application/exception/BadAgentNumberGuessException.java
+++ src/main/java/application/exception/BadAgentNumberGuessException.java
@@ -1,4 +1,14 @@
 package application.exception;
 
-public class BadAgentNumberGuessException extends Throwable {
+public class BadAgentNumberGuessException extends Exception {
+    private final int agentNumberGuess;
+
+    public BadAgentNumberGuessException(int agentNumberGuess) {
+
+        this.agentNumberGuess = agentNumberGuess;
+    }
+
+    public int getAgentNumberGuess() {
+        return agentNumberGuess;
+    }
 }
